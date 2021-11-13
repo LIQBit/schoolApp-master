b65be2c696340955a433f581836bbb883499cbaa
Leigh Bitossi <66171363+LIQBit@users.noreply.github.com>
2021-11-13 18:00:14 +0900

    fixed: AddStudent input fields reset to blank now

--- client/src/Components/AddStudent.js
+++ client/src/Components/AddStudent.js
@@ -1,34 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { getClassesQuery, addStudentMutation, getStudents } from "../queries/index";
+import { selectionSetMatchesResult } from "@apollo/client/cache/inmemory/helpers";
 
 const AddStudent = () => {
   const { data, loading, error } = useQuery(getClassesQuery);
-
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
   const [classId, setClassId] = useState("");
   const [test, setTest] = useState("");
   const [addStudent] = useMutation(addStudentMutation);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addStudent({
+      variables: {
+        name,
+        age,
+        classId: classId,
+        test1: test,
+      },
+      refetchQueries: [{ query: getStudents}]
+    });
+    setName("");
+    setAge("");
+    setTest("");
+    setClassId("");
+    
+  };
+
+    
+
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error!</p>;
+
+  
 
   return (
     <form
       id="add-student"
-      onSubmit={(e) => {
-        e.preventDefault();
-        addStudent({
-          variables: {
-            name,
-            age,
-            classId: classId,
-            test1: test,
-          },
-          refetchQueries: [{ query: getStudents}]
-        });
-      }}
+      onSubmit={handleSubmit}
     >
       <div className="field">
         <label>Student Name:</label>
--- client/src/Components/__tests__/delete.test.js
+++ client/src/Components/__tests__/delete.test.js
@@ -1,6 +1,7 @@
 import TestRenderer from 'react-test-renderer';
 import { MockedProvider } from '@apollo/client/testing';
 import DeleteStudent from '../DeleteStudent';
+import deleteStudentMutation from '../../queries/index';
 
 it('should render without errors', () => {
     TestRenderer.create(
